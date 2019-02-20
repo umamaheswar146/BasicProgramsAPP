48b3f308a28cddf16386fd0b2ee54107fcac0e3c
umamaheswar146 <47803847+umamaheswar146@users.noreply.github.com>
2019-02-20 17:03:50 +0530

    first

---
+++ arraydemo.java
@@ -0,0 +1,16 @@
+package com.regnant;
+
+public class arraydemo {
+
+	public static int[] array(int [] y){
+   for (int i=0;i<y.length;i++) 
+   {
+	
+	      y[i]=y[i]+2;
+	
+   }
+
+   return y;
+	}
+
+}
